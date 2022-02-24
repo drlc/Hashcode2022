dd8140d246d50dafdb6d346f1e46ee058efe9b44
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2022-02-24 22:03:10 +0100

    first solution

--- src/base_practice/entities/entities.py
+++ src/base_practice/entities/entities.py
@@ -11,6 +11,8 @@ class Skill(BaseModel):
 class Contributor(BaseModel):
     name: str
     skills: List[Skill] = []
+    hired_until: int = -1
+
 
 
 class Role(BaseModel):
@@ -24,3 +26,38 @@ class Proj(BaseModel):
     score: int
     best_before: int
     roles: List[Role] = []
+    end_day: int = None
+    hires: List[Contributor] = []
+
+    def hire(self, contributor_by_skills, current_day) -> List[Contributor]:
+        hired = []
+        end_project_day = current_day + self.days -1
+        try:
+            print(f"Hiring for {self.name} day {current_day}===")
+            for role in self.roles:
+                print(f"Looking for {role.name}")
+                print(contributor_by_skills[role.name])
+                contrib_el = next(filter(lambda x: x[1].level >= role.level and x[0].hired_until <= current_day, contributor_by_skills[role.name]))
+                contrib = contrib_el[0]
+                hired.append((contrib, role))
+            self.hires = [x[0] for x in hired]
+            self.end_day = end_project_day
+        except Exception:
+            print(f"hiring exception for {self.name}")
+            return []
+
+        for hire, role in hired:
+            print(f"HIRE: {hire.name} untill {end_project_day}")
+            hire.hired_until = end_project_day
+            hire_role = next(x for x in hire.skills if x.name == role.name)
+            if hire_role.level == role.level:
+                print(f"{hire.name} levelup {role.name}")
+                hire_role.level += 1
+
+        return hired
+
+    def __str__(self):
+        return f"{self.name}\n{' '.join([x.name for x in self.hires])}"
+
+
+
--- src/base_practice/main.py
+++ src/base_practice/main.py
@@ -1,37 +1,36 @@
+from collections import defaultdict
+import datetime
 from base_practice.entities.entities import Contributor, Skill, Proj, Role
 from base_practice.io_ops import IOOps
-
-file_names = [
-    "a_an_example.in.txt",
-    "b_better_start_small.in.txt",
-    "c_collaboration.in.txt",
-    "d_dense_schedule.in.txt",
-    "e_exceptional_skills.in.txt",
-    "f_find_great_mentors.in.txt"
-]
+from itertools import count
 
 
-def main():
+
+def main(filename):
     io_ops = IOOps()
     contributors = []
     projects = []
-    file_name = "a_an_example.in.txt"
+    contributor_by_skills = defaultdict(list)
 
     # parse the file
-    (num_contr, num_proj), lines = io_ops.read_file(file_name)
+    (num_contr, num_proj), lines = io_ops.read_file(filename)
     index_c = 0
-    for i in range(num_contr):
+    for _ in range(num_contr):
         c = lines[index_c]
         con = Contributor(name=c[0])
         num_skills = int(c[1])
         for j in range(num_skills):
             line = lines[index_c + j + 1]
-            con.skills.append(Skill(name=line[0], level=int(line[1])))
+            skill_name = line[0]
+            skill_lvl = int(line[1])
+            skill = Skill(name=skill_name, level=skill_lvl)
+            con.skills.append(skill)
+            contributor_by_skills[skill_name].append((con, skill))
         index_c += num_skills
         contributors.append(con)
         index_c += 1
 
-    for i in range(num_proj):
+    for _ in range(num_proj):
         p = lines[index_c]
         proj = Proj(name=p[0], days=int(p[1]), score=int(p[2]), best_before=int(p[3]))
         num_roles = int(p[4])
@@ -44,3 +43,38 @@ def main():
         index_c += 1
 
     print("parsed")
+
+    plan = []
+    end_all_ongoing_projects = -1
+    for i in count(0):
+        print(f"day {i}")
+        chosen = next((x for x in projects if x.hire(contributor_by_skills, i)), None)
+        print(chosen)
+        if not chosen:
+            if end_all_ongoing_projects < i:
+                break
+            continue
+        projects.remove(chosen)
+        assert chosen.end_day > 0
+        assert len(chosen.hires) > 0
+        end_all_ongoing_projects = max(chosen.end_day, end_all_ongoing_projects)
+        plan.append(chosen)
+    
+    print(plan)
+    # output
+    ts = datetime.datetime.now().timestamp()
+    io_ops.write_objects_in_file(f"{filename}{ts}.out", plan)
+    
+
+
+if __name__ == "__main__":
+    file_names = [
+        "a_an_example.in.txt",
+        # "b_better_start_small.in.txt",
+        # "c_collaboration.in.txt",
+        # "d_dense_schedule.in.txt",
+        # "e_exceptional_skills.in.txt",
+        # "f_find_great_mentors.in.txt"
+    ]
+    for f in file_names:
+        main(f)
